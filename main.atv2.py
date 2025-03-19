93c8b5c41fac167d3895f32339986e2362f3c794
wsp99x <dihhhd71@icloud.com>
2025-03-19 13:34:16 -0300

    Exercicios Crorrigidos

--- main.atv2.py
+++ main.atv2.py
@@ -2,7 +2,7 @@ from modulos.soma_notas import calcular_soma
 from modulos.media_alunos import calcular_media
 
 aluno = input("Digite o nome do aluno: ")
-notas = float(input("Digite as notasdo aluno: "))
+notas = list(map(float, input("Digite as notas separadas por espaço: ").split()))
 soma = calcular_soma(notas)
 media = calcular_media(soma, len(notas))
 print(f"A média das notas do aluno {aluno} é: {media:.2f}")
